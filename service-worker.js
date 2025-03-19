e9c5ad25f0fa979d85fb010b63e9041fe1ce0175
Nefertari1216 <3568901553@qq.com>
2025-03-19 10:58:40 +0800

    初始提交

---
+++ service-worker.js
@@ -0,0 +1,74 @@
+const CACHE_NAME = '2048game-v1';
+const urlsToCache = [
+  '/2048game/',
+  '/2048game/index.html',
+  '/2048game/manifest.json',
+  '/2048game/icons/icon-72x72.png',
+  '/2048game/icons/icon-96x96.png',
+  '/2048game/icons/icon-128x128.png',
+  '/2048game/icons/icon-144x144.png',
+  '/2048game/icons/icon-152x152.png',
+  '/2048game/icons/icon-192x192.png',
+  '/2048game/icons/icon-384x384.png',
+  '/2048game/icons/icon-512x512.png'
+];
+
+// 安装Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('缓存已打开');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// 激活Service Worker
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// 拦截请求并从缓存中响应
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // 如果找到缓存的响应，则返回缓存的值
+        if (response) {
+          return response;
+        }
+        
+        // 否则发起网络请求
+        return fetch(event.request)
+          .then(response => {
+            // 检查是否收到有效响应
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            
+            // 克隆响应
+            const responseToCache = response.clone();
+            
+            // 将响应添加到缓存
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+              
+            return response;
+          });
+      })
+  );
+});
