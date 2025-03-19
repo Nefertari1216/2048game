d5dcf323cf02023c3d0a934de42f1dbd9319eb17
Nefertari1216 <3568901553@qq.com>
2025-03-19 11:07:03 +0800

    更新说明

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,16 @@
 const CACHE_NAME = '2048game-v1';
 const urlsToCache = [
-  '/2048game/',
-  '/2048game/index.html',
-  '/2048game/manifest.json',
-  '/2048game/icons/icon-72x72.png',
-  '/2048game/icons/icon-96x96.png',
-  '/2048game/icons/icon-128x128.png',
-  '/2048game/icons/icon-144x144.png',
-  '/2048game/icons/icon-152x152.png',
-  '/2048game/icons/icon-192x192.png',
-  '/2048game/icons/icon-384x384.png',
-  '/2048game/icons/icon-512x512.png'
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-72x72.png',
+  './icons/icon-96x96.png',
+  './icons/icon-128x128.png',
+  './icons/icon-144x144.png',
+  './icons/icon-152x152.png',
+  './icons/icon-192x192.png',
+  './icons/icon-384x384.png',
+  './icons/icon-512x512.png'
 ];
 
 // 安装Service Worker
